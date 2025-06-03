52d35eac03704efba92720d963f76b3048578d2d
Dana Kun <dana.liepinsh@gmail.com>
2025-06-03 09:47:21 +0200

    feat: add template

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,3 @@
-import { logo } from '../constants/assets.js';
-
 /**
  * Renders the main application content
  * @returns {void}
@@ -7,19 +5,7 @@ import { logo } from '../constants/assets.js';
 export function renderApp() {
   document.querySelector('#app').innerHTML = `
     <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="${logo.vite}" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${logo.js}" class="logo vanilla" alt="JavaScript logo" />
-      </a>
-      <h1>Vanilla JS Template</h1>
-      <div class="card">
-        <button id="counter" type="button"></button>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-      </p>
+      <h1>Vanilla JS Starter Template</h1>
     </div>
   `;
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './styles/style.css';
-import { setupCounter } from './components/counter.js';
 import { renderApp } from './components/app.js';
 
 // Initialize the application
 renderApp();
-
-// Setup counter functionality
-setupCounter(document.querySelector('#counter'));
